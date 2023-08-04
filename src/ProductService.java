4a7a4e09ecbad04f5391c02f67bc867e012b57c2
sirisha469 <chsiri469@gmail.com>
2023-08-04 20:34:05 +0530

    product

--- src/ProductService.java
+++ src/ProductService.java
@@ -12,4 +12,13 @@ public class ProductService {
     public List<Product> getAllProducts(){
         return products;
     }
+
+    public Product getProduct(String name){
+        for(Product p:products){
+            if(p.getName().equals(name)){
+                return p;
+            }
+        }
+        return null;
+    }
 }
