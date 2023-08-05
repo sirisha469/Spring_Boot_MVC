12d859ad8b8120154ef660442a2cd4a3f071a1fe
sirisha469 <chsiri469@gmail.com>
2023-08-05 13:12:08 +0530

    using-database

---
+++ src/main/java/ProductService.java
@@ -0,0 +1,53 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductService {
+
+    List<Product> products = new ArrayList<>();
+    ProductDB db = new ProductDB();
+
+    public void addProduct(Product p){
+        //products.add(p);
+
+        db.save(p);
+    }
+
+    public List<Product> getAllProducts(){
+        return db.getAll();
+    }
+//
+//    public Product getProduct(String name){
+//        for(Product p:products){
+//            if(p.getName().equals(name)){
+//                return p;
+//            }
+//        }
+//        return null;
+//    }
+//
+//    public List<Product> getProductByPlace(String place){
+//        List<Product> placeList = new ArrayList<>();
+//        for(Product p: products){
+//            if(p.getPlace().equals(place)){
+//                placeList.add(p);
+//            }
+//        }
+//        return placeList;
+//    }
+//
+//    public List<Product> getProductWithText(String text) {
+//        String str = text.toLowerCase();
+//        List<Product> textProd = new ArrayList<>();
+//
+//        for(Product p: products){
+//            String name = p.getName().toLowerCase();
+//            String type = p.getType().toLowerCase();
+//            String place = p.getPlace().toLowerCase();
+//            if(name.contains(str) || type.contains(str) || place.contains(str)){
+//                textProd.add(p);
+//            }
+//        }
+//        return textProd;
+//    }
+}
